876f2d9dfafac07c5d2e20a6fa8605d4e53e10c5
Matej Minarik <mato.minarik@azet.sk>
2017-10-05 14:46:08 +0200

    Initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { File, Entry } from '@ionic-native/file';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,38 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  dataDirectory: string = '';
+  applicationDirectory: string = '';
+  createdFile: string = '';
+  copiedFile: string = '';
+  resultError: string = '';
 
+  constructor(
+    public navCtrl: NavController,
+    private file: File
+  ) { }
+
+  ionViewDidEnter() {
+    const fileName = (new Date()).getMilliseconds().toString();
+    this.dataDirectory = this.file.dataDirectory;
+    this.applicationDirectory = this.file.applicationDirectory;
+
+    this.file.createFile(
+      this.file.dataDirectory,
+      fileName,
+      true
+    ).then((entry: Entry) => {
+      this.createdFile = entry.nativeURL;
+      return this.file.copyFile(
+        this.file.dataDirectory,
+        fileName,
+        this.file.dataDirectory,
+        `${fileName}_copy`
+      )
+    }).then((entry: Entry) => {
+      this.copiedFile = entry.nativeURL;
+    }).catch(err => {
+      this.resultError = JSON.stringify(err);
+    });
   }
-
 }
